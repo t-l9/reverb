963af812ba935191a525c928f78b19855cfb15fd
Tim Lauter <timlauter@0-0.local>
2016-12-03 16:08:20 -0500

    added sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable and :omniauthable
+    devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+    has_many :team_members
+    has_many :teams, through: :team_members
+    has_many :messages
 end
